7f8fdb7439576ac6bc6af67f922e1bc2ad976e49
Harlan <harchandler@yahoo.com>
2022-03-07 19:40:50 -0500

    Created Branch, MainPage, MainPageController, main-page-view, and the css file to go along with it

---
+++ src/main/java/com/schwabarista/baristaview/MainPageController.java
@@ -0,0 +1,21 @@
+package com.schwabarista.baristaview;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class MainPageController {
+
+    @FXML
+    public ListView MainListView;
+
+    @FXML
+    public Button QuitButton;
+
+    @FXML
+    public void QuitButtonClicked (ActionEvent e) {
+
+    }
+
+}
