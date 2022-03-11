fec30fdff5f54eef00cfa450918213a99db5da20
Hunter Kalinoski <HMK30@pitt.edu>
2022-03-11 15:45:44 -0500

    imported useful files from the other project

---
+++ src/main/java/com/schwabarista/baristaview/core/controllers/CoffeeCellFactory.java
@@ -0,0 +1,18 @@
+package com.schwabarista.baristaview.core.controllers;
+
+
+import com.schwabarista.baristaview.core.controllers.CoffeeCell;
+import com.schwabarista.baristaview.models.CoffeeModel;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+public class CoffeeCellFactory implements Callback<ListView<CoffeeModel>, ListCell<CoffeeModel>> {
+
+    @Override
+    public ListCell<CoffeeModel> call(ListView<CoffeeModel> param) {
+        return new CoffeeCell();
+    }
+
+
+}
