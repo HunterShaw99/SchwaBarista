3b1f84414bda53e8ea45ed4701193d5b186927d2
Harlann Chandler <HEC60@pitt.edu>
2022-03-14 13:59:32 -0400

    added aws dependencies and imports to MainPageController and pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,9 @@ module com.schwabarista.baristaview {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires aws.java.sdk.sqs;
+    requires aws.java.sdk.core;
+    requires software.amazon.awssdk.regions;
 
     opens com.schwabarista.baristaview to javafx.fxml;
     exports com.schwabarista.baristaview;
